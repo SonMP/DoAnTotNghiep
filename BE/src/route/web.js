bf15d516285f5be17641ba2f389bd24dc2616ecd
Son MP <thaison1962003@gmail.com>
2025-12-11 23:27:41 +0700

    base CRUD and base frontend

--- BE/src/route/web.js
+++ BE/src/route/web.js
@@ -6,6 +6,13 @@ let router = express.Router();
 let initWebRoutes = (app) => {
 
     router.get("/", homeController.getHomePage);
+    router.get("/crud", homeController.displayCRUD);
+
+    router.post("/post-crud", homeController.postCRUD);
+    router.get("/get-crud", homeController.getCRUD);
+    router.get("/edit-crud", homeController.getEditCRUD);
+    router.post("/put-crud", homeController.putCRUD);
+    router.get("/delete-crud", homeController.deleteCRUD);
 
     return app.use("/", router);
 }
